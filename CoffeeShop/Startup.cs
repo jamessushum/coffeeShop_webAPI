930db9ce0066702ee753b0e352fc7ca83886da30
James Su-Shum <sushumjames@gmail.com>
2020-09-29 17:50:59 -0400

    Added JS files to test Web API endpoints

--- CoffeeShop/Startup.cs
+++ CoffeeShop/Startup.cs
@@ -37,6 +37,14 @@ namespace CoffeeShop
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // Do not block requests while in development
+                app.UseCors(options =>
+                {
+                    options.AllowAnyOrigin();
+                    options.AllowAnyMethod();
+                    options.AllowAnyHeader();
+                });
             }
 
             app.UseHttpsRedirection();
