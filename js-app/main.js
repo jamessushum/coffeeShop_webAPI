9d74614f88d263caa600e959249244112c35a5d0
James Su-Shum <sushumjames@gmail.com>
2020-09-30 12:36:04 -0400

    Added JS code to enable users to add a new bean to database

--- js-app/main.js
+++ js-app/main.js
@@ -11,3 +11,25 @@ button.addEventListener("click", () => {
 function getAllBeanVarieties() {
   return fetch(url).then(resp => resp.json());
 }
+
+// Adding new bean
+document.getElementById("add-bean-button").addEventListener("click", async () => {
+  const newBean = {
+    Name: document.getElementById("bean-name").value,
+    Region: document.getElementById("bean-region").value,
+    Notes: document.getElementById("bean-notes").value
+  }
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newBean)
+  })
+  const result = await res.json();
+  return result.then(() => {
+    document.getElementById("new-bean-form").reset();
+  });
+
+})
